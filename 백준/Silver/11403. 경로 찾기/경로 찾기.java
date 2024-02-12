8e052ffbb03ca011234a23ff5f131b878ae3652a
Hun425 <147483675+Hun425@users.noreply.github.com>
2024-02-13 00:17:33 +0900

    [Silver I] Title: 경로 찾기, Time: 332 ms, Memory: 16552 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -27,13 +27,15 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (arr[i][j]!=0) {
+                if (arr[i][j]!=0) { // 원래 i==j일때 0으로 초기화 해야하지만
+                                    // 구해야하는 값이 최단경로가 아닌 연결된 경로이므로 자기자신일때 초기화안함
                     distance[i][j]=arr[i][j];
                 } else {
                     distance[i][j]=Integer.MAX_VALUE/2-1;
                 }
             }
         }
+        // 플로이드-워셜 공식
 
         for (int k = 0; k < N; k++) {
             for (int S = 0; S < N; S++) {
