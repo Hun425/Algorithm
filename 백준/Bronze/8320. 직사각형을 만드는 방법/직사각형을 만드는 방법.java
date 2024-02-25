3e5325f867fc1dc4ae20ddeafb05c303adeaf431
Hun425 <147483675+Hun425@users.noreply.github.com>
2024-02-25 17:14:03 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 156 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static boolean check1, check2;
+    static boolean Babygin1(int[] arr,boolean check) {
+        int [] DAT = new int[10];
+
+        for (int i = 0; i < arr.length-2; i++) {
+            if(arr[i]==-1)continue;
+            DAT[arr[i]]++;
+        }
+        for (int i = 0; i < DAT.length; i++) {
+            if (DAT[i] >= 3) {
+                check = true;
+                return check;
+
+            }
+        }
+        return check;
+    }
+    static boolean Babygin2(int[] arr,boolean check) {
+
+        for (int i = 0; i < arr.length-2; i++) {
+            if(arr[i]==-1)continue;
+            if (arr[i] == arr[i + 1] + 1 ) {
+                if (arr[i + 1] == arr[i + 2] + 1) {
+                    check = true;
+                    return check;
+                }
+            } else if (arr[i] == arr[i + 1] - 1) {
+                if (arr[i + 1] == arr[i + 2] - 1) {
+                    check =true;
+                    return check;
+                }
+            }
+        }
+
+        return check;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int result = N;
+        for (int i = 2; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                if(i*j<=N)result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
